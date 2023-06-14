5785c511738bd78f0c2ed361696d0307cc4acc7a
TILLO <Alimovtillo@gmail.com>
2023-06-14 16:36:41 +0600

    init project

---
+++ core/settings/dev.py
@@ -0,0 +1,15 @@
+from core.settings.base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
---
+++ core/settings/prod.py
@@ -0,0 +1,19 @@
+from core.settings.base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USERNAME'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'PORT': config('PORT'),
+        'HOST': config('HOST'),
+    }
+}
